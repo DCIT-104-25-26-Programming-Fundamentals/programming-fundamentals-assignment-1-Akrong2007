c7c7079083753055549e951d31f2d76f59a2db76
Eben Akrong <eakrong010@st.ug.edu.gh>
2026-07-28 16:35:32 +0000

    Main block of code was added

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -42,3 +42,11 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+# Main block
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is NOT a prime number.")
