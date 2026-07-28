d674e591762cee10c48318b159f07f9d4ba62729
Eben Akrong <eakrong010@st.ug.edu.gh>
2026-07-28 17:00:28 +0000

    Main block of code was added

--- assignment_02_student_grade_system.py
+++ assignment_02_student_grade_system.py
@@ -59,3 +59,12 @@ def get_grade(score):
         return "D"
     else:
         return "F"
+
+    # Main block
+score = int(input("Enter student score (0-100): "))
+grade = get_grade(score)
+
+if grade is None:
+    print("Error: Score must be between 0 and 100.")
+else:
+    print(f"Grade: {grade}")
